16c9f65d5b279e058c076c1fb050a67a4cef4cfb
PalinaBerazavikova <Palina_Berazavikova@epam.com>
2016-09-28 16:10:11 +0300

    1

--- MethodsPoint/MethodsPoint/Point.cs
+++ MethodsPoint/MethodsPoint/Point.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace MethodsPoint
 {
-    class Point
+    public class Point
     {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+
+        public bool Equals(Point point)
+        {
+            return ((this.X == point.X) && (this.Y == point.Y));
+        }
+
+        public override string ToString()
+        {
+            return $"X: {this.X}; Y: {this.Y}";
+        }
     }
+
+    
 }
--- MethodsPoint/MethodsPoint/Program.cs
+++ MethodsPoint/MethodsPoint/Program.cs
@@ -10,6 +10,16 @@ namespace MethodsPoint
     {
         static void Main(string[] args)
         {
+            Point point = new Point(123.23, 4564.23);
+            Point point1 = new Point(123.23, 4564.23);
+            Point point2 = new Point(123, 4564);
+
+            Console.WriteLine(point.Equals(point1));
+            Console.WriteLine(point.Equals(point2));
+            Console.WriteLine(point.ToString());
+            Console.WriteLine(point2.ToString());
+            Console.ReadKey();
+
         }
     }
 }
